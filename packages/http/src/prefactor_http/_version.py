b73fc61fdcec8222545130cde5aa80f05e595c70
Mukil Chittybabu <mukil033@gmail.com>
2026-05-18 12:12:43 +1000

    chore: bump package versions

--- packages/http/src/prefactor_http/_version.py
+++ packages/http/src/prefactor_http/_version.py
@@ -3,5 +3,5 @@
 from __future__ import annotations
 
 PACKAGE_NAME = "prefactor-http"
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 PACKAGE_VERSION = __version__
--- packages/langchain/src/prefactor_langchain/_version.py
+++ packages/langchain/src/prefactor_langchain/_version.py
@@ -1,5 +1,5 @@
 """Package version for prefactor-langchain."""
 
 PACKAGE_NAME = "prefactor-langchain"
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 PACKAGE_VERSION = __version__
